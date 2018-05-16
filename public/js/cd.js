e593a8ee5de4d27d53ed4555a3608fb6fe1f9598
Andrés Gar <andresgar@gars-macbook.zte.com.cn>
2018-05-16 02:07:21 -0500

    carrito agregado

--- public/js/cd.js
+++ public/js/cd.js
@@ -1,1 +1,19 @@
-var precioBtn = document.getElementById('precioBtn');
+// carrito
+document.querySelectorAll('.agregar').forEach(function(button) {
+    button.addEventListener('click', function(){
+        var id = button.parentNode.getAttribute('data-id');
+        console.log(button.parentNode.parentNode.getAttribute('data-id'));
+
+        if(arreglo.indexOf(id) >= 0){
+            return;
+        }
+
+        if(arreglo.indexOf(id) >= -1){
+            button.innerHTML = "-";
+        }
+
+        arreglo.push(id);
+        update();
+        localStorage.setItem('arreglo', JSON.stringify(arreglo));
+    });
+});
